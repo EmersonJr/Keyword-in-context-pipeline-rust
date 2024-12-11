159b96e8ef6d43598cbf040192f5afb37a08de28
EMERSON LUIZ CRUZ JUNIOR <emersonjr02@gmail.com>
2024-12-11 12:45:20 -0300

    fix: adding a struct to solve my complexity problems

--- kic/src/genWordsAndText.rs
+++ kic/src/genWordsAndText.rs
@@ -1,17 +1,25 @@
+use std::collections::VecDeque;
+
+pub struct wordContext {
+
+    pub first: VecDeque<String>,
+    pub second: VecDeque<String>,
+}
+
 pub mod genWordsAndText {
 
     use std::fs::File;
     use std::io::{self, BufRead, BufReader};
     use std::collections::HashMap;
 
-    pub fn genWordsAndText(stopWords : HashMap<String, i32>) -> Result<HashMap<,>, io::Error> {
+    use super::wordContext;
 
-        let mut wordsAndContext: HashMap<&str, Vec<&str>> = HashMap::new();
+    pub fn genWordsAndText(stopWords : HashMap<String, i32>) -> Result<HashMap<String, wordContext>, io::Error> {
+
+        let mut wordsAndContext: HashMap<String, wordContext> = HashMap::new();
 
         let file = File::open("./source/texts.txt")?;
         let reader = BufReader::new(file);
-
-        wordsAndContext.insert("AAAA", vec!["AAA", "BBBB", "CCC"]);
         
         for line in reader.lines() {
 
