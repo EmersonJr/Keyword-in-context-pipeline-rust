22abeffb1d0b77f4102f171c292ed02c444490fd
EMERSON LUIZ CRUZ JUNIOR <emersonjr02@gmail.com>
2024-12-11 21:08:55 -0300

    feat: defining the contexts of the keywords

--- kic/src/genWordsAndText.rs
+++ kic/src/genWordsAndText.rs
@@ -1,35 +1,80 @@
 use std::collections::VecDeque;
+#[derive(Clone)]  
+pub struct WordContext {
 
-pub struct wordContext {
-
-    pub first: VecDeque<String>,
-    pub second: VecDeque<String>,
+    pub phrase: VecDeque<String>,
+    pub wher: i32,
 }
 
 pub mod genWordsAndText {
-
+    
     use std::fs::File;
     use std::io::{self, BufRead, BufReader};
-    use std::collections::HashMap;
+    use std::collections::{HashMap, VecDeque};
+    use std::cmp;
+    use super::WordContext;
 
-    use super::wordContext;
+    pub fn genWordsAndText(stopWords : HashMap<String, i32>) -> Result<Vec<WordContext>, io::Error> {
 
-    pub fn genWordsAndText(stopWords : HashMap<String, i32>) -> Result<HashMap<String, wordContext>, io::Error> {
-
-        let mut wordsAndContext: HashMap<String, wordContext> = HashMap::new();
+        let mut wordsAndContext: Vec<WordContext> = Vec::new();
 
         let file = File::open("./source/texts.txt")?;
         let reader = BufReader::new(file);
-        
+
+        let mut cnt = 0;
+        let mut tamanJanela = 0;
+
         for line in reader.lines() {
 
-            let mut frase = line.unwrap().split(' ').collect();
+            let mut linha = line.unwrap();
 
-            println!("{}", frase);
+            if cnt == 0 {
+                
+                tamanJanela = linha.trim().parse::<i32>().unwrap() as usize;
+                cnt += 1;
+                continue;
+            }
 
-            // wordsAndContext.insert(frase[0].to_string())
+            linha = format!("{} {}", linha, linha);
+
+            let mut frase : Vec<&str> = linha.as_str().split(' ').collect();
+
+            let mut act = WordContext {
+                phrase: VecDeque::new(),
+                wher: cnt,
+            };
+            
+            let mut aux = 0;
+
+            for palavra in &frase {
+
+                if aux >= (frase.len() / 2) {
+
+                    break;
+                }
+
+                act.phrase.push_back(String::from(*palavra));
+
+                if act.phrase.len() == cmp::min(tamanJanela, frase.len() / 2) {
+
+                    let num = match stopWords.get(act.phrase.front().unwrap()) {
+                        Some(x)  => *x,
+                        None => 0,
+                    };
+    
+                    if num == 0 {
+    
+                        wordsAndContext.push(act.clone());
+                    }
+
+                    act.phrase.pop_front();
+                    aux += 1;
+                }
+            }
+
+            cnt += 1;
         }
 
-        Ok(wordsAndContext);
+        Ok(wordsAndContext)
     }
 }
--- kic/src/main.rs
+++ kic/src/main.rs
@@ -1,8 +1,8 @@
 // defining the functions to the pipeline
 // 
 // stopWordsDefining() -> HashMap of stop words and a Boolean value -> check
-// TODO:genWordsAndText() -> hashmap of words and text (String) taken from the file,
-// using the stop words
+// genWordsAndText() -> hashmap of words and text (String) taken from the file,
+// using the stop words -> check
 // TODO:sortInsideHash() -> sorting the hashmap of words and text
 // TODO:sortAndPrinting() -> sorting keys of the hashmap and printing them
 
@@ -10,17 +10,22 @@ mod defStopWords;
 mod genWordsAndText;
 
 use defStopWords::defStopWords::stopWordsDefining;
+use genWordsAndText::genWordsAndText::genWordsAndText;
 
 fn main() {
 
-    let aux = match stopWordsDefining() {
+    let aux = match genWordsAndText(stopWordsDefining().unwrap()) {
         Ok(stopWords) => stopWords,
         Err(s) => panic!("{}", s),
     };
 
-    for (key, val)  in &aux {
+    for act in aux {
 
-        println!("{}, {}", key, val);
+        for palavra in act.phrase {
+
+            print!("{} ", palavra);
+        }
+
+        println!();
     }
-
 }
